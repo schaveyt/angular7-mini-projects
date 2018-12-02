1441372eb038db432a67e2b08e0bfcc558c3682e
Todd Schavey <schaveyt@gmail.com>
2018-12-02 11:52:30 -0500

    app: routing; example of adding query params and frames to the route (programmatically)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,8 @@ export class HomeComponent implements OnInit {
   }
 
   // This example shows how to route programmatically
-  onLoadServers() {
-    this.router.navigate(['/servers']);
+  onLoadServers(id: number) {
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'somefragment'});
   }
 
 }
